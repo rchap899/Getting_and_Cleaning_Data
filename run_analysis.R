242088004ac6b0563ad34a5b358ee178733b709c
rchap899 <rajclaxmi@gmail.com>
2015-07-26 10:07:27 -0400

    Getting and Clening data project

---
+++ run_analysis.R
@@ -0,0 +1,71 @@
+#1. Merges the training and the test sets to create one data set.
+#2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#3. Uses descriptive activity names to name the activities in the data set
+#4. Appropriately labels the data set with descriptive variable names. 
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Install Packages data.table, reshape2 
+# 
+
+# Test data
+# read activity labels
+activity_labels <- read.table("./activity_labels.txt")[,2]
+
+# read List of all features 
+features <- read.table("./features.txt")[,2]
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+extract_features <- grepl("mean|std", features)
+
+# read and process X_test & y_test data.
+X_test <- read.table("./test/X_test.txt")
+y_test <- read.table("./test/y_test.txt")
+subject_test <- read.table("./test/subject_test.txt")
+
+names(X_test) = features
+# Extract only the measurements on the mean and standard deviation for each measurement from X_test test set.
+X_test = X_test[,extract_features]
+
+# Load test activity labels
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+
+names(subject_test) = "subject"
+
+# Bind test data
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+
+
+# Training data 
+# read and process X_train & y_train data.
+X_train <- read.table("./train/X_train.txt")
+y_train <- read.table("./train/y_train.txt")
+subject_train <- read.table("./train/subject_train.txt")
+
+names(X_train) = features
+# Extract only the measurements on the mean and standard deviation for each measurement from X_train trainig set.
+X_train = X_train[,extract_features]
+
+# Load training activity labels
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+
+names(subject_train) = "subject"
+
+# Bind training data
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+
+
+# Merge test and train data
+data = rbind(test_data, train_data)
+
+id_labels   = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+
+# Apply mean function to dataset using dcast function
+tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+
+write.table(tidy_data, file = "./tidy_data.txt")
+
+
